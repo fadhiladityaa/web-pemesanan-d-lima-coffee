83695ac6a740b787fbea1ed4de15d2b2f154dd4e
fadhiladityaa <fadhiladitya551@gmail.com>
2025-01-14 09:58:26 +0800

    Memperbaiki responsive untuk ukuran tablet

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,40 +1,141 @@
-<div class="bg-black mt-0 text-white font-poppins pt-16 flex items-center flex-col pb-32">
-    <div class="w-52 text-sm text-center">
-        <h1 class="text-center font-bold text-lg "><span class="text-primary ">Menu</span> Kami</h1>
-        <h2 class="font-extralight text-ssm mt-2">Memastikan menu yang tersedia adalah yang terbaik</h2>
+<div class="bg-black  mt-0 text-white font-poppins pt-16 flex items-center flex-col pb-32">
+    <div class="w-52 sm:w-72 text-sm text-center">
+        <h1 class="text-center font-bold text-lg  text-white sm:text-3xl "><span class="text-primary ">Menu</span> Kami</h1>
+        <h2 class="font-extralight text-ssm sm:text-mmd sm:mb-3 mt-2">Memastikan menu yang tersedia adalah yang terbaik</h2>
     </div>
 
-    <div class="card mt-5 bg-secondary font-poppins text-menu font-bold flex flex-row gap-4 pt-4 pl-4 pr-6 pb-4">
-        <div class="kiri w-24 flex flex-col justify-center">
-            <img class="w-24" src="img/kopi-1.png" alt="">
-
-            
-            <div class="counter text-black flex mx-auto mt-4 font-normal">
-                <button class="px-2.5 py-0 border border-slate-500 rounded-lg">-</button>
-                <p class="px-2.5 py-0 border border-slate-500 rounded-lg">0</p>
-                <button class="px-2.5 py-0 border border-slate-500 rounded-lg">+</button>
+    <div class="card-container  mt-5 flex flex-col sm:flex-row sm:flex-wrap sm:items-center sm:justify-center sm:gap-3">
+        <div class="card bg-secondary font-poppins text-menu font-bold flex flex-row gap-4 pt-4 pl-4 pr-6 pb-4">
+            <div class="kiri w-24 flex flex-col justify-center">
+                <img class="w-24" src="img/kopi-1.png" alt="">
+    
+                
+                <div class="counter text-black flex mx-auto mt-4 font-normal">
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">-</button>
+                    <p class="px-2.5 py-0 border border-slate-500 rounded-lg">0</p>
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">+</button>
+                </div>
+            </div>
+    
+            <div class="kanan w-44">
+                <div class="nama-menu flex justify-between mt-1.5">
+                    <h3>Cappucino</h3>
+                    <p class="text-primary">7k</p>
+                </div>
+    
+                <div class="deskripsi text-ssm font-extralight mt-1">
+                    <p>Perpaduan sempurna antara espresso, susu hangat, dan foam lembut di atasnya.</p>
+                </div>
+    
+                <div class="level text-ssm text-black font-light flex gap-x-1.5 mt-2">
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Sweet</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Medium</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Strong</span>
+                </div>
+    
+                <button class="bg-primary font-normal w-full rounded-2xl py-2 mt-3 text-white hover:bg-amber-800">
+                    Add To Cart
+                </button>
             </div>
         </div>
-
-        <div class="kanan w-44">
-            <div class="nama-menu flex justify-between mt-1.5">
-                <h3>Cappucino</h3>
-                <p class="text-primary">7k</p>
+        <div class="card bg-secondary font-poppins text-menu font-bold flex flex-row gap-4 pt-4 pl-4 pr-6 pb-4">
+            <div class="kiri w-24 flex flex-col justify-center">
+                <img class="w-24" src="img/kopi-1.png" alt="">
+    
+                
+                <div class="counter text-black flex mx-auto mt-4 font-normal">
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">-</button>
+                    <p class="px-2.5 py-0 border border-slate-500 rounded-lg">0</p>
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">+</button>
+                </div>
             </div>
-
-            <div class="deskripsi text-ssm font-extralight mt-1">
-                <p>Perpaduan sempurna antara espresso, susu hangat, dan foam lembut di atasnya.</p>
+    
+            <div class="kanan w-44">
+                <div class="nama-menu flex justify-between mt-1.5">
+                    <h3>Cappucino</h3>
+                    <p class="text-primary">7k</p>
+                </div>
+    
+                <div class="deskripsi text-ssm font-extralight mt-1">
+                    <p>Perpaduan sempurna antara espresso, susu hangat, dan foam lembut di atasnya.</p>
+                </div>
+    
+                <div class="level text-ssm text-black font-light flex gap-x-1.5 mt-2">
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Sweet</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Medium</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Strong</span>
+                </div>
+    
+                <button class="bg-primary font-normal w-full rounded-2xl py-2 mt-3 text-white hover:bg-amber-800">
+                    Add To Cart
+                </button>
             </div>
-
-            <div class="level text-ssm text-black font-light flex gap-x-1.5 mt-2">
-                <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Sweet</span>
-                <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Medium</span>
-                <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Strong</span>
+        </div>
+        <div class="card bg-secondary font-poppins text-menu font-bold flex flex-row gap-4 pt-4 pl-4 pr-6 pb-4">
+            <div class="kiri w-24 flex flex-col justify-center">
+                <img class="w-24" src="img/kopi-1.png" alt="">
+    
+                
+                <div class="counter text-black flex mx-auto mt-4 font-normal">
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">-</button>
+                    <p class="px-2.5 py-0 border border-slate-500 rounded-lg">0</p>
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">+</button>
+                </div>
             </div>
-
-            <button class="bg-primary font-normal w-full rounded-2xl py-2 mt-3 text-white hover:bg-amber-800">
-                Add To Cart
-            </button>
+    
+            <div class="kanan w-44">
+                <div class="nama-menu flex justify-between mt-1.5">
+                    <h3>Cappucino</h3>
+                    <p class="text-primary">7k</p>
+                </div>
+    
+                <div class="deskripsi text-ssm font-extralight mt-1">
+                    <p>Perpaduan sempurna antara espresso, susu hangat, dan foam lembut di atasnya.</p>
+                </div>
+    
+                <div class="level text-ssm text-black font-light flex gap-x-1.5 mt-2">
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Sweet</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Medium</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Strong</span>
+                </div>
+    
+                <button class="bg-primary font-normal w-full rounded-2xl py-2 mt-3 text-white hover:bg-amber-800">
+                    Add To Cart
+                </button>
+            </div>
+        </div>
+        <div class="card bg-secondary font-poppins text-menu font-bold flex flex-row gap-4 pt-4 pl-4 pr-6 pb-4">
+            <div class="kiri w-24 flex flex-col justify-center">
+                <img class="w-24" src="img/kopi-1.png" alt="">
+    
+                
+                <div class="counter text-black flex mx-auto mt-4 font-normal">
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">-</button>
+                    <p class="px-2.5 py-0 border border-slate-500 rounded-lg">0</p>
+                    <button class="px-2.5 py-0 border border-slate-500 rounded-lg">+</button>
+                </div>
+            </div>
+    
+            <div class="kanan w-44">
+                <div class="nama-menu flex justify-between mt-1.5">
+                    <h3>Cappucino</h3>
+                    <p class="text-primary">7k</p>
+                </div>
+    
+                <div class="deskripsi text-ssm font-extralight mt-1">
+                    <p>Perpaduan sempurna antara espresso, susu hangat, dan foam lembut di atasnya.</p>
+                </div>
+    
+                <div class="level text-ssm text-black font-light flex gap-x-1.5 mt-2">
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Sweet</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Medium</span>
+                    <span class="bg-white px-2 py-1 rounded-xl hover:bg-primary hover:text-white">Strong</span>
+                </div>
+    
+                <button class="bg-primary font-normal w-full rounded-2xl py-2 mt-3 text-white hover:bg-amber-800">
+                    Add To Cart
+                </button>
+            </div>
         </div>
     </div>
 </div>
