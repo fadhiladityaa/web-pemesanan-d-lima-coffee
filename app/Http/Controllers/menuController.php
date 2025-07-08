3af48c9e50bd1a35bb8553e65aadcb59b3b4d24e
fadhiladityaa <fadhiladitya551@gmail.com>
2025-07-08 20:46:14 +0800

    rapihkan sedikit

--- app/Http/Controllers/menuController.php
+++ app/Http/Controllers/menuController.php
@@ -5,13 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Daftar_menu;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+// use Illuminate\Support\Facades\Redis;
 
 class menuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('dashboard.menu-management', [
@@ -20,9 +17,6 @@ class menuController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('dashboard.menu-create', [
@@ -30,9 +24,6 @@ class menuController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         // dd($request);
@@ -61,9 +52,6 @@ class menuController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Daftar_menu $daftar_menu)
     {
         // dd($daftar_menu);
@@ -76,9 +64,6 @@ class menuController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Daftar_menu $daftar_menu)
     {
         $rules = ([
