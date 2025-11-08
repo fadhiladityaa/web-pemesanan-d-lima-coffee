b876aac7ef54c7ad40012381298dd813b643663d
fadhiladityaa <fadhiladitya551@gmail.com>
2025-11-09 07:49:10 +0800

    memperbaiki navbar agar sesuai desain figma

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,8 @@
-<nav class="navbar bg-black shadow-lg flex justify-between z-40 lg:p-4 fixed">
-    <div class="title">
-        <a href="#beranda" class="btn btn-ghost text-xl lg:text-2xl text-white font-poppins font-bold">
-            D'Lima<span class="text-primary italic">Coffee<span class="text-white">.</span></span>
+<nav class="navbar bg-[#947257] shadow-lg flex justify-between z-40 lg:p-4 fixed">
+    <div class="font-[Sriracha] pl-[16px] flex gap-2 items-center">
+        <img src="{{ asset('img/logo-warkop 1.svg') }}" alt="logo-warkop" class="w-10 lg:w-12 mt-1">
+        <a href="#beranda" class="text-xl lg:text-2xl text-white">
+            D'Lima Coffee.</span></span>
         </a>
     </div>
 
@@ -12,74 +13,48 @@
         <a href="">Kontak</a>
     </div>
 
-            <!-- Dropdown Content -->
-            <div class="card card-compact absolute bg-base-100 z-[1] mt-3 -right-12 w-96 shadow" x-show="!open" x-cloak x-transition @click.outside="open = !open">
+    <!-- Dropdown Content -->
+    <div class="card card-compact absolute bg-base-100 z-[1] mt-3 -right-12 w-96 shadow" x-show="!open" x-cloak
+        x-transition @click.outside="open = !open">
+    </div>
+    </div>
 
-                <!-- Keranjang -->
-                <div class="card-body flex flex-col justify-between">
-                    <div class="topper flex flex-row-reverse items-end gap-2">
-                        <button @click="open = !open"
-                            class="font-bold text-lg border border-black-100 px-2 hover:bg-slate-500">
-                            X
-                        </button>
-                        <p class="">Keranjang Anda</p>
-                    </div>
 
-                    
-            
-                    
-                    <div class="checkout flex  items-center ">
-                        <span class="bg-yellow-300 w-full p-2 rounded-sm">Total: <span x-text="totalPrice"></span></span>
-                        <form method="POST" action="{{ route('checkout.store') }}">
-                            @csrf
-                            <input type="hidden" name="menu" :value="JSON.stringify(cart)">
-                            <input type="hidden" name="username" value="{{ auth()->user()->name }}">
-                            <input type="hidden" name="userphone" value="{{ auth()->user()->noHp }}">
-                            <button type="submit">Buat pesanan</button>
-                        </form>
-                    </div>
-                </div>
-                {{-- end keranjang --}}
+    <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-7 rounded-full">
+                <img alt="hamburger icon" src="{{ asset('img/burger-menu.svg') }}" />
             </div>
         </div>
+        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li>
+                <a href="/profile" class="justify-between">
+                    Profile
+                </a>
+            </li>
+            <li><a>Settings</a></li>
 
+            <li class="sm:hidden">
+                <a href="/tentang-kami">Tentang Kami</a>
+            </li>
 
-        <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                <div class="w-9 rounded-full">
-                    <img alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
-            </div>
-            <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li>
-                    <a href="/profile" class="justify-between">
-                        Profile
-                    </a>
-                </li>
-                <li><a>Settings</a></li>
+            <li class="sm:hidden">
+                <a href="">Kontak</a>
+            </li>
 
-                <li class="sm:hidden">
-                    <a href="/tentang-kami">Tentang Kami</a>
-                </li>
+            <li class="">
+                <a href="/dashboard">Dashboard</a>
+            </li>
 
-                <li class="sm:hidden">
-                    <a href="">Kontak</a>
-                </li>
+            <li>
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </li>
 
-                <li class="">
-                    <a href="/dashboard">Dashboard</a>
-                </li>
-
-                <li>
-                    <form action="/logout" method="POST">
-                        @csrf
-                        <button type="submit">Logout</button>
-                    </form>
-                </li>
-
-            </ul>
-        </div>
+        </ul>
+    </div>
     </div>
     </div>
 </nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,45 +2,11 @@
 
 @section('container')
     {{-- hero section --}}
-    <div class="hero-section  font-poppins relative flex bg-center sm:bg-bottom pt-14 items-start lg:h-screen lg:bg-bottom ">
-        <img src="{{ asset('img/hero.png') }}" class="lg:bg-cover relative brightness-90 -z-10 lg:-top-32" alt="">
-        <div
-            class="text pt-12 sm:pt-[7rem] pl-7 text-xl absolute font-semibold text-white w-72 sm:text-3xl sm:w-2/3 lg:text-5xl lg:pt-28 lg:leading-tight">
-            <h1>Sambut hangatnya <span class="text-primary sm:text-red-300">ngopi</span> bersama kami</h1>
-            <p class="font-extralight text-sm sm:text-lg lg:text-3xl">Rasakan hangatnya kopi dimanapun anda berada</p>
-            <a class="text-sm bg-primary px-5 py-2 rounded-lg mt-3 absolute sm:text-lg lg:mt-5 lg:text-2xl"
-                href="/#menu">
-                Beli Sekarang
-            </a>
-        </div>
-    </div>
+   
     {{-- end hero section --}}
 
     {{-- about section --}}
-    <div class="bg-black -mt-1 font-poppins pt-14 pb-8 text-white flex-col sm:items-center">
-        <h1 class="text-lg font-bold text-center text-white sm:text-3xl">
-            <span class="text-primary">Tentang</span> Kami
-        </h1>
-
-        <div class="content flex flex-col items-center p-3 gap-2 sm:flex-row sm:justify-center sm:gap-6">
-            <div class="img-wrapper pt-1">
-                <img class="w-60 sm:w-72" src="img/about.png" alt="">
-            </div>
-            <div class="text-wrapper flex flex-col items-start gap-1 w-60 sm:w-72 mt-7 text-justify">
-                <h2 class="text-sm text-center sm:text-xl">
-                    Kenapa Memilih Kopi Kami?
-                </h2>
-                <p class="font-extralight text-ssm sm:text-mmd sm:text-justify">
-                    karena dipilih langsung dari biji kopi berkualitas tinggi,
-                </p>
-
-                <p class="font-extralight text-ssm sm:text-justify sm:text-mmd">
-                    memastikan setiap pelanggan tidak hanya menikmati kopi yang lezat, tetapi juga memahami kandungannya
-                    dan memilih kopi yang tepat untuk kebutuhan pelanggan.
-                </p>
-            </div>
-        </div>
-    </div>
+    
     {{-- end about section --}}
 
     {{-- menu section --}}
