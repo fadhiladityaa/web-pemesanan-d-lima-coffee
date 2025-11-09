50809ec16b3e39a991cefc59b8747c09bc6ef8f7
fadhiladityaa <fadhiladitya551@gmail.com>
2025-11-09 10:54:21 +0800

    memperbaiki tampilan input catalog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,16 @@
     {{-- Location Section --}}
     <section>
         <div class="container w-full h-80 bg-[#2C2C2C] pt-20 font-poppins text-white">
-            <div class="location-grup flex gap-2 flex-col px-[16px]">
-                <span>Location</span>
+            <div class="location-grup flex flex-col px-[16px]">
+                <span class="text-[18px]">Location</span>
                 <span>Jalan Delima, Parepare</span>
-                <div class="input-gorup flex">
+                <div class="input-gorup flex items-center gap-2  mt-6">
                     <fieldset class="fieldset">
-                        <input type="text" class="input" placeholder="Type here" />
+                        <input type="text" class="input w-[300px]" placeholder="Type here" />
                     </fieldset>
-                    <button>Cari</button>
+                    <button class="bg-[#947257] w-[54px] h-[45px] flex justify-center items-center rounded-md">
+                        <img src="{{ asset('img/settings2-svgrepo-com.svg') }}" class="w-7 h-7" alt="search icon" />
+                    </button>
                 </div>
                 {{-- <div class="w-full border border-black flex justify-center mt-6"> --}}
                     <div class="kotak w-full h-40 bg-white rounded-md border mt-10 border-black">
