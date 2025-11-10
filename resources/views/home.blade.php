235c08656080aa7a91c396578a3e7d6a13cf9e94
fadhiladityaa <fadhiladitya551@gmail.com>
2025-11-10 16:11:40 +0800

    memperbaiki tampilan untuk desktop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,80 +16,72 @@
                     </button>
                 </div>
                 <div class="kotak w-full h-40 sm:h-64 bg-white shadow-md rounded-[10px] border mt-10"></div>
-            </section>
-            {{-- End Location Section --}}
+    </section>
+    {{-- End Location Section --}}
 
 
     {{-- daftar menu section --}}
-     <section>
-            <div class=" mt-28 sm:mt-44 w-full px-[16px] sm:px-[32px] font-poppins">
-                <div class="category-section text-black flex gap-5 text-ssm sm:text-lg mt-6">
-                    <div class="semua-menu bg-primary px-4 py-1 rounded-[10px] shadow-soft text-white">semua menu</div>
-                    <div class="semua-menu px-4 py-1 rounded-[10px] text-slate-700 shadow-soft">makanan</div>
-                    <div class="semua-menu px-4 py-1 rounded-[10px] text-slate-700 shadow-soft">minuman</div>
+    <section>
+        <div class=" mt-28 sm:mt-44 w-full px-[16px] sm:px-[32px] font-poppins">
+            <div class="category-section text-black flex gap-5 text-ssm sm:text-lg mt-6">
+                <div class="semua-menu bg-primary px-4 py-1 rounded-[10px] shadow-soft text-white">semua menu</div>
+                <div class="semua-menu px-4 py-1 rounded-[10px] text-slate-700 shadow-soft">makanan</div>
+                <div class="semua-menu px-4 py-1 rounded-[10px] text-slate-700 shadow-soft">minuman</div>
+            </div>
+    </section>
+
+    <section class="px-[16px] sm:px-[32px]">
+        <div class="category-makanan text-black mt-10">
+            <h2 class="text-2xl sm:text-3xl text-slate-800">makanan</h2>
+            <div class="card-container grid grid-cols-2 gap-6 mt-6 w-full">
+    </section>
+
+<div class="container-asli lg:grid lg:grid-cols-3">
+    <section class="grid grid-cols-2 lg:col-span-2 px-[16px] gap-2 sm:px-[32px]">
+        @foreach ($menus as $item)
+            {{-- item container --}}
+            <div class="font-poppins flex flex-col items-start rounded-lg shadow-soft sm:p-5 p-3">
+                <div class="w-full relative rounded-lg overflow-hidden sm:h-48">
+                    <img src="{{ asset('img/contoh-kopi.png') }}"
+                        class="w-full sm:rounded-lg sm:h-48 sm:object-cover hover:scale-110 transition-all duration-500"
+                        alt="">
                 </div>
-                <div class="category-makanan text-black mt-10">
-                    <h2 class="text-2xl sm:text-3xl text-slate-800">makanan</h2>
-                    <div class="card-container grid grid-cols-2 gap-6 mt-6 w-full">
+                <span class="text-[14px] sm:text-[20px] text-slate-800 sm:mt-2 mt-1">{{ $item->nama_menu }}</span>
+                <span class="text-primary text-[12px] sm:text-[17px] font-bold">Rp. {{ $item->harga }}</span>
+                <span class="text-[12px] sm:text-[18px] text-black/50">{{ $item->deskripsi }}</span>
 
-                        @foreach ($menus as $item) 
-                        {{-- item container --}}
-                        <div class="font-poppins flex flex-col items-start rounded-lg shadow-soft sm:p-5 p-3">
-                            <div class="w-full relative rounded-lg overflow-hidden sm:h-48">
-                                <img src="{{ asset('img/contoh-kopi.png') }}" class="w-full sm:rounded-lg sm:h-48 sm:object-cover hover:scale-110 transition-all duration-500" alt="">
-                            </div>
-                            <span class="text-[14px] sm:text-[20px] text-slate-800 sm:mt-2 mt-1">{{ $item->nama_menu }}</span>
-                            <span class="text-primary text-[12px] sm:text-[17px] font-bold">Rp. {{ $item->harga }}</span>
-                            <span class="text-[12px] sm:text-[18px] text-black/50">{{ $item->deskripsi }}</span>
-
-                            <div class="button  flex flex-col sm:gap-[10px] gap-[5px] w-full mt-2">
-                                <button
-                                class="text-[12px] sm:text-[18px] border sm:border-2 border-[#CE8F69]/50 w-full p-[5px] hover:bg-primary text-slate-800 font-light hover:text-white transition-all duration-500 rounded-[4px]">Lihat
-                                detail menu</button>
-                                <button
-                                class="text-[12px] sm:text-[18px] text-white bg-primary w-full p-[5px] font-light rounded-[4px] hover:bg-yellow-800 transition-colors duration-500">Tambah</button>
-                            </div>
-                        </div>
-                        {{-- end item container --}}
-                        @endforeach
-                      
-
-                    </div>
+                <div class="button  flex flex-col sm:gap-[10px] gap-[5px] w-full mt-2">
+                    <button
+                        class="text-[12px] sm:text-[18px] border sm:border-2 border-[#CE8F69]/50 w-full p-[5px] hover:bg-primary text-slate-800 font-light hover:text-white transition-all duration-500 rounded-[4px]">Lihat
+                        detail menu</button>
+                    <button
+                        class="text-[12px] sm:text-[18px] text-white bg-primary w-full p-[5px] font-light rounded-[4px] hover:bg-yellow-800 transition-colors duration-500">Tambah</button>
                 </div>
             </div>
-      </section>
-      {{-- end daftar menu section --}}
+            {{-- end item container --}}
+        @endforeach
+    </section>
+    {{-- end daftar menu section --}}
 
-      {{-- keranjang section --}}
-      <section>
-        <div class="w-full px-[16px] sm:px-[32px] mt-9 font-poppins mb-[100px]">
+    {{-- keranjang section --}}
+    <section>
+        <div class="w-full lg:w-auto px-[16px] sm:px-[32px] mt-9 lg:mt-0 font-poppins lg:sticky lg:top-">
             <div class="cart-container w-full shadow-medium rounded-md p-[16px]">
                 <div class="topper-container flex justify-between">
                     <h2 class="sm:text-[1.7rem]">Keranjang</h2>
-                    <span class="px-8 py-[.2rem] border border-primary rounded-2xl sm:text-lg text-ssm text-primary font-bold">2 Items</span>
+                    <span
+                        class="px-8 py-[.2rem] border border-primary rounded-2xl sm:text-lg text-ssm text-primary font-bold">2
+                        Items</span>
                 </div>
                 <div class="cart-items-container mt-4">
                     {{-- cart item --}}
-                    <div class="items-container bg-[#E8E8E8] mt-2 sm:mt-4 shadow-md rounded-md text-slate-800 p-3 flex justify-between">
+                    <div
+                        class="items-container bg-[#E8E8E8] mt-2 sm:mt-6 shadow-md rounded-md text-slate-800 p-3 flex justify-between">
                         <img src="{{ asset('img/contoh-kopi-2.png') }}" class="sm:w-36" alt="contoh kopi">
                         <div class="pricing-container flex sm:mt-1 sm:mr-[15rem] flex-col gap-2">
                             <span class="text-[18px] sm:text-[1.4rem]">Espresso</span>
-                            <span class="text-[14px]  sm:text-[1.3rem]">20.000 x 1 = <span class="text-primary">Rp. 20.0000</span></span>
-                            <div class="counter-container flex gap-2 mt-2">
-                                <span class="px-3 sm:px-5 sm:py-[0.1rem] sm:text-lg rounded-[4px] bg-[#CACACA]">+</span>
-                                <span class="sm:text-lg">1</span>
-                                <span class="px-3 sm:px-5 sm:py-[0.1rem] sm:text-lg rounded-[4px] bg-[#CACACA]">-</span>
-                            </div>
-                        </div>
-                        <span class="text-red-500 sm:text-3xl">×</span>
-                    </div>
-                    {{-- end cart item --}}
-                    {{-- cart item --}}
-                    <div class="items-container bg-[#E8E8E8] mt-2 sm:mt-4 shadow-md rounded-md text-slate-800 p-3 flex justify-between">
-                        <img src="{{ asset('img/contoh-kopi-2.png') }}" class="sm:w-36" alt="contoh kopi">
-                        <div class="pricing-container flex sm:mt-1 sm:mr-[15rem] flex-col gap-2">
-                            <span class="text-[18px] sm:text-[1.4rem]">Espresso</span>
-                            <span class="text-[14px]  sm:text-[1.3rem]">20.000 x 1 = <span class="text-primary">Rp. 20.0000</span></span>
+                            <span class="text-[14px]  sm:text-[1.3rem]">20.000 x 1 = <span class="text-primary">Rp.
+                                    20.0000</span></span>
                             <div class="counter-container flex gap-2 mt-2">
                                 <span class="px-3 sm:px-5 sm:py-[0.1rem] sm:text-lg rounded-[4px] bg-[#CACACA]">+</span>
                                 <span class="sm:text-lg">1</span>
@@ -118,12 +110,14 @@
                             <span class="text-primary font-bold">Rp. 40.000</span>
                         </div>
 
-                        <button class="w-full bg-green-500 py-2 rounded-md text-white hover:bg-green-600 transition-all sm:text-2xl sm:mt-2  duration-300">Checkout</button>
+                        <button
+                            class="w-full bg-green-500 py-2 rounded-md text-white hover:bg-green-600 transition-all sm:text-2xl sm:mt-2  duration-300">Checkout</button>
                     </div>
 
                 </div>
             </div>
         </div>
-      </section>
-      {{-- end keranjang section --}}
+    </section>
+    {{-- end keranjang section --}}
+ </div>
 @endsection
