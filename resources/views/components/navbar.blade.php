f7fecb3ae87b87cf3db6b6a000d9152cbe26e7cc
fadhiladityaa <fadhiladitya551@gmail.com>
2025-11-10 12:49:36 +0800

    memperbaiki container asal

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="bg-[#947257] w-full flex justify-between shadow-lg z-40 fixed">
-    <div class="font-[Sriracha] flex items-center justify-between mx-[16px] sm:mx-[32px] gap-2 py-2 w-full">
+    <div class="font-[Sriracha] flex items-center justify-between mx-[16px] sm:mx-[32px] gap-2 py-3 w-full">
         {{-- logo section --}}
         <div class="flex items-center gap-2">
-            <img src="{{ asset('img/logo-warkop 1.svg') }}" alt="logo-warkop" class="w-10 lg:w-12 mt-1">
-            <a href="#beranda" class="text-xl lg:text-2xl text-white">
+            <img src="{{ asset('img/logo-warkop 1.svg') }}" alt="logo-warkop" class="w-10 sm:w-11 lg:w-12 mt-1">
+            <a href="#beranda" class="text-xl sm:text-2xl lg:text-2xl text-white">
                 D'Lima Coffee.</span>
             </a>
         </div>
@@ -13,7 +13,14 @@
             <div tabindex="0" role="button" class="lg:hidden sm:hidden">
                 <div class="w-7 rounded-full">
                     <img alt="hamburger icon" src="{{ asset('img/burger-menu.svg') }}" />
+
                 </div>
+            </div>
+            <div class="links text-white sm:flex gap-7 sm:text-[20px] items-center hidden font-poppins">
+                <a class="{{ Request()->is('/#beranda') ? 'text-primary font-bold' : '' }}" href="/">Menu</a>
+                <a class="" href="/berita">Edukasi</a>
+                <a class="{{ Request()->is('/#menu') ? 'text-primary font-bold' : '' }}" href="#menu">About</a>
+                <a href="">Berita</a>
             </div>
             <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                 <li>
@@ -43,12 +50,6 @@
                 </li>
 
             </ul>
-        </div>
-        <div class="links text-white sm:flex gap-7 items-center hidden font-poppins">
-            <a class="{{ Request()->is('/#beranda') ? 'text-primary font-bold' : '' }}" href="/">Menu</a>
-            <a class="" href="/berita">Edukasi</a>
-            <a class="{{ Request()->is('/#menu') ? 'text-primary font-bold' : '' }}" href="#menu">About</a>
-            <a href="">Berita</a>
         </div>
     </div>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,13 @@
 @section('container')
     {{-- Location Section --}}
     <section>
-        <div class="container w-full h-[340px] sm:h-[390px] bg-[#2C2C2C] pt-20 font-poppins text-white">
+        <div class="w-full h-[340px] sm:w-full sm:h-[390px] bg-[#2C2C2C] pt-20 font-poppins text-white">
             <div class="location-grup flex mt-4 flex-col px-[16px] sm:px-[32px]">
-                <span class="text-[18px]">Location</span>
-                <span>Jalan Delima, Parepare</span>
+                <span class="text-[18px] sm:text-[24px]">Location</span>
+                <span class="sm:text-[18px]">Jalan Delima, Parepare</span>
                 <div class="input-gorup flex items-center gap-2  mt-6">
                     <fieldset class="fieldset">
-                        <input type="text" class="input w-[300px]" placeholder="Type here" />
+                        <input type="text" class="input w-[300px] sm:w-[400px]" placeholder="Type here" />
                     </fieldset>
                     <button class="bg-[#C67C4E] w-[54px] h-[45px] flex justify-center items-center rounded-md">
                         <img src="{{ asset('img/settings2-svgrepo-com.svg') }}" class="w-7 h-7" alt="search icon" />
@@ -22,32 +22,32 @@
 
     {{-- daftar menu section --}}
      <section>
-            <div class="container mt-28 sm:mt-40 w-full px-[16px] sm:px-[32px]">
-                <div class="category-section text-black flex gap-5 text-ssm mt-6">
+            <div class=" mt-28 sm:mt-44 w-full px-[16px] sm:px-[32px] font-poppins text-slate-700">
+                <div class="category-section text-black flex gap-5 text-ssm sm:text-lg mt-6">
                     <div class="semua-menu bg-primary px-4 py-1 rounded-[10px] shadow-soft text-white">Semua menu</div>
                     <div class="semua-menu px-4 py-1 rounded-[10px] shadow-soft">Makanan</div>
                     <div class="semua-menu px-4 py-1 rounded-[10px] shadow-soft">Minuman</div>
                 </div>
                 <div class="category-makanan text-black mt-10">
-                    <h2 class="text-2xl text-slate-800">Makanan</h2>
+                    <h2 class="text-2xl sm:text-3xl text-slate-800">makanan</h2>
                     <div class="card-container grid grid-cols-2 gap-6 mt-6 w-full">
 
                         @foreach ($menus as $item) 
                         {{-- item container --}}
-                        <div class="container font-poppins flex flex-col items-start rounded-lg shadow-soft sm:p-5 p-3">
+                        <div class="font-poppins flex flex-col items-start rounded-lg shadow-soft sm:p-5 p-3">
                             <div class="w-full relative rounded-lg overflow-hidden sm:h-48">
                                 <img src="{{ asset('img/contoh-kopi.png') }}" class="w-full sm:rounded-lg sm:h-48 sm:object-cover hover:scale-110 transition-all duration-500" alt="">
                             </div>
-                            <span class="text-[14px] sm:text-[18px] text-slate-800 mt-1">{{ $item->nama_menu }}</span>
+                            <span class="text-[14px] sm:text-[20px] text-slate-800 sm:mt-2 mt-1">{{ $item->nama_menu }}</span>
                             <span class="text-primary text-[12px] sm:text-[17px] font-bold">Rp. {{ $item->harga }}</span>
                             <span class="text-[12px] sm:text-[18px] text-black/50">{{ $item->deskripsi }}</span>
 
                             <div class="button  flex flex-col gap-[5px] w-full mt-2">
                                 <button
-                                class="text-[12px] border border-[#CE8F69]/50 w-full p-[5px] hover:bg-primary text-slate-800 font-light hover:text-white transition-all duration-500 rounded-[4px]">Lihat
+                                class="text-[12px] sm:text-[18px] border sm:border-2 border-[#CE8F69]/50 w-full p-[5px] hover:bg-primary text-slate-800 font-light hover:text-white transition-all duration-500 rounded-[4px]">Lihat
                                 detail menu</button>
                                 <button
-                                class="text-[12px] text-white bg-primary w-full p-[5px] font-light rounded-[4px] hover:bg-yellow-800 transition-colors duration-500">Tambah</button>
+                                class="text-[12px] sm:text-[18px] text-white bg-primary w-full p-[5px] font-light rounded-[4px] hover:bg-yellow-800 transition-colors duration-500">Tambah</button>
                             </div>
                         </div>
                         {{-- end item container --}}
@@ -62,7 +62,7 @@
 
       {{-- keranjang section --}}
       <section>
-        <div class="container w-full px-[16px] sm:px-[32px] mt-9 font-poppins mb-[100px]">
+        <div class="w-full px-[16px] sm:px-[32px] mt-9 font-poppins mb-[100px]">
             <div class="cart-container w-full shadow-medium rounded-md p-[16px]">
                 <div class="topper-container flex justify-between">
                     <h2>Keranjang</h2>
