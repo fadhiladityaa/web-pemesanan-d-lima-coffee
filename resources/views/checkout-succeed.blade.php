a411b7f66f3667304dad13b19377c0a9119b9deb
fadhiladityaa <fadhiladitya551@gmail.com>
2025-11-17 22:42:34 +0800

    memperbaiki padding pada halaman success

--- resources/views/checkout-succeed.blade.php
+++ resources/views/checkout-succeed.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="text-center pt-28">
+<div class="text-center pt-24">
     <h1 class="text-2xl font-bold text-green-600">Checkout Berhasil!</h1>
     <p class="mt-4 text-slate-700">Terima kasih atas pesanan Anda. Kami sedang memprosesnya.</p>
     <a href="{{ route('all.about.home') }}" class="mt-6 inline-block px-4 py-2 bg-primary text-white rounded">Kembali ke Beranda</a>
