a3a791c373a769734c3e4fb6e44f6672f363d724
fadhiladityaa <fadhiladitya551@gmail.com>
2025-12-09 15:03:09 +0800

    membuat 3 user untuk admin di adminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::insert(
+            [
+                'name' => 'Fadhil Aditya',
+                'noHp' => '085756956684',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ],
+            [
+                'name' => 'Muhammad Aqmal Nurfauzi',
+                'noHp' => '081243010760',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ],
+            [
+                'name' => 'M. Faiz Ilyas',
+                'noHp' => '087734377501',
+                'password' => bcrypt('password'),
+                'role' => 'admin'
+            ]
+        );
     }
 }
