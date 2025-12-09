1335314e35ee0ede68e3d8e7cb47a3e034bf4dda
fadhiladityaa <fadhiladitya551@gmail.com>
2025-12-09 15:08:50 +0800

    rapihkan pada halaman web.php

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\menuController;
-use App\Models\Daftar_menu;
-// use Faker\Guesser\Name;
-// use App\Http\Controllers\Controller;
-use App\Http\Controllers\beritaController;
 use App\Livewire\CreateMenu;
 use App\Livewire\DashboardAdmin;
 use App\Livewire\DetailMenu;
@@ -39,35 +34,8 @@ Route::post('/logout', [loginController::class, 'logout'])->name('logout');
 // route untuk profile
 Route::get('/profile', [ProfileController::class, 'index'])->middleware(('auth'));
 
-// Route::get('/menu', [menuController::class, 'index'])->name('menu.utama');
-
-// Route ke halaman dashboard
-// Route::get('/dashboard', function() {
-//     return view('dashboard.index', [
-//         'title' => 'dasbor',
-//     ]);
-// });
-
-// Route untuk crud menu
-// Route::get('/dashboard/menu-management', [menuController::class, 'index'])->name('menu.index');
-// Route::get('/dashboard/{daftar_menu:nama_menu}/edit', [menuController::class, 'edit'])->name('menu.edit');
-// Route::put('/dashboard/{daftar_menu}', [menuController::class, 'update'])->name('menu.update');
-// Route::post('/tambah-menu', [menuController::class, 'store'])->name('menu.store');
-// Route::delete('/menu/{daftar_menu}', [menuController::class, 'destroy'])->name('menu.destroy');
-// Route::get('/dashboard/menu/create', [menuController::class, 'create'])->name('menu.create');
-
-// Route untuk crud berita (kali)
-// Route::get('/detail-pesanan/{order}', [DetailPesanan::class])->name('detail.pesanan');
-// Route::get('/pesanan-masuk/detail-pesanan/{order}', function() { return view('detail-pesanan-view'); })->name('detail.pesanan');
-Route::get('/pesanan-masuk/detail-pesanan/{order}', \App\Livewire\DetailPesanan::class)
+Route::get('/pesanan-masuk/detail-pesanan/{order}', DetailPesanan::class)
     ->name('detail.pesanan');
-
-
-// route untuk fitur add to cart
-// Route::post('/add-to-cart', [CartController::class, 'store'])->name('cart.store');
-
-
-// Route::get('/detail-menu', [menuController::class, 'show']);
 
 Route::get('/checkout', function() {
     return view('checkout-view', ['title' => 'Halaman Checkout']);
@@ -77,12 +45,6 @@ Route::get('/checkout', function() {
 Route::get('/checkout-succees', function() {
     return view('checkout-succeed', ['title' => 'checkout-success']);
 })->name('checkout.success');
-
-
-
-// Route::get('/pesanan-masuk', function() {
-//     return view('dashboard.pesanan-masuk-view',['title' => 'Pesanan Masuk']);
-// })->name('pesanan.masuk');
 
 Route::get('/profil-pengguna', function() {
     return view('profil-pengguna', ['title' => 'profil-pengguna']);
@@ -95,17 +57,13 @@ Route::get('/Login', function() {
 
 Route::get('/dashboard/create-menu', CreateMenu::class);
 Route::get('/dashboard-admin', DashboardAdmin::class);
-// Route::get('/detail-menu', function(){
-//     return view('detail-menu', ['title' => 'Detail Menu']);
-// });
-
 Route::get('/dashboard/menu-management', MenuManagement::class)->name('dashboard.menu.management');
 Route::get('/dashboard/{id}/edit', EditMenu::class)->name('dashboard.edit.menu');
 Route::get('/dashboard/pesanan-masuk', PesananMasuk::class);
+Route::get('/dashboard/edukasi-management', Edukasi::class)->name('dashboard.edukasi.management');
 
 
 Route::get('/menu/detail-menu/{daftar_menus}', DetailMenu::class)->name('detail.menu');
-
 Route::get('/pesanan-saya', PesananSaya::class)->name('pesanan.saya');
 
 
@@ -115,7 +73,6 @@ Route::get('/pesanan-saya', PesananSaya::class)->name('pesanan.saya');
 Route::get('/edukasi', \App\Livewire\EdukasiPelanggan::class)->name('edukasi');
 
 //  EDUKASI MANAGEMENT UNTUK ADMIN (tidak berubah)
-Route::get('/dashboard/edukasi-management', \App\Livewire\Edukasi::class)->name('dashboard.edukasi.management');
 
 //  Route untuk tombol "Lihat Halaman Pelanggan" di admin
 Route::get('/edukasi-pelanggan', function() {
