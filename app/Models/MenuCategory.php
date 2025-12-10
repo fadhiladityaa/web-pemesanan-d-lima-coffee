a7c7f1a13100ac13e008958b7cbc861b7df359e8
fadhiladityaa <fadhiladitya551@gmail.com>
2025-12-10 17:18:29 +0800

    menambahkan relasi pada model menu category

--- app/Models/MenuCategory.php
+++ app/Models/MenuCategory.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Daftar_menu;
 use Illuminate\Database\Eloquent\Model;
 
 class MenuCategory extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function daftar_menus()
+    {
+        return $this->hasMany(Daftar_menu::class);
+    }
 }
