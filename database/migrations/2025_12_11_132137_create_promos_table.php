879472fa646697c12a15297ab3680ad8b0c8302e
fadhiladityaa <fadhiladitya551@gmail.com>
2025-12-30 11:53:41 +0800

    fix dikit lagi

--- database/migrations/2025_12_11_132137_create_promos_table.php
+++ database/migrations/2025_12_11_132137_create_promos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('promos', function (Blueprint $table) {
             $table->id();
             // Pastikan nama kolom ini 'judul', bukan 'title' atau 'name'
+            $table->string('kode_promo');
             $table->string('judul');
             $table->text('deskripsi')->nullable();
             $table->integer('persentase_diskon');
