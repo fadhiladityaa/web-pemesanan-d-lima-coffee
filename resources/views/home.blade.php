84d69e10e8024bcbcbcfd4d8c1d4019f6c536360
fadhiladityaa <fadhiladitya551@gmail.com>
2026-01-06 00:59:19 +0800

    perbaiki panjang kotak

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     {{-- Location Section --}}
     <section>
-        <div x-data="{ info: 'Location' }" class="w-full h-[340px] sm:h-[390px] lg:h-[500px] bg-[#2C2C2C] pt-20 font-poppins text-white">
+        <div x-data="{ info: 'Location' }" class="w-full h-[340px] sm:h-[390px] lg:h-[470px] bg-[#2C2C2C] pt-20 font-poppins text-white">
             <div class="location-grup flex flex-col px-[16px] sm:px-[32px] lg:px-[64px] mt-4">
                 <span x-text="info" class="text-[18px] sm:text-[24px]"></span>
                 <span class="sm:text-[18px]">Jalan Delima, Parepare</span>
