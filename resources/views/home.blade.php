3be1819e07203351c11eeff2e8de2f647184e8bd
fadhiladityaa <fadhiladitya551@gmail.com>
2026-01-06 00:58:11 +0800

    perbaiki banner di halaman catalog menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,17 @@
 @section('container')
     {{-- Location Section --}}
     <section>
-        <div x-data="{ info: 'Location' }" class="w-full h-[340px] sm:h-[390px] bg-[#2C2C2C] pt-20 font-poppins text-white">
+        <div x-data="{ info: 'Location' }" class="w-full h-[340px] sm:h-[390px] lg:h-[500px] bg-[#2C2C2C] pt-20 font-poppins text-white">
             <div class="location-grup flex flex-col px-[16px] sm:px-[32px] lg:px-[64px] mt-4">
                 <span x-text="info" class="text-[18px] sm:text-[24px]"></span>
                 <span class="sm:text-[18px]">Jalan Delima, Parepare</span>
+                <!-- Container dengan aspect ratio 16:9 -->
                 <div
-                    class="kotak w-full h-40 sm:h-64 bg-white rounded-xl shadow-lg border border-gray-200 mt-[7rem] overflow-hidden hover:shadow-2xl transition-shadow duration-300">
-                    {{-- <img src="{{ asset('img/banner-dlima.png') }}" alt="Banner Lokasi" class="w-full h-full lg:h-20  object-cover"> --}}
+                    class="kotak w-full lg:w-4/5 lg:mx-auto h-40 lg:h-96 sm:h-64 bg-white rounded-xl shadow-lg border border-gray-200 mt-[7rem] overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                    <img src="{{ asset('img/promo_5.png') }}" alt="Banner Lokasi"
+                        class="w-full h-full object-cover object-center" <!-- object-center bisa diganti: object-top,
+                        object-bottom, object-left, dll -->
+                    >
                 </div>
 
             </div>
